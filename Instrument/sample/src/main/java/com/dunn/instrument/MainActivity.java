881cd9d486263904286112c61a82115d4f010c4e
zhuyidian <zhuyidian@coocaa.com>
2022-03-20 15:37:20 +0800

    过滤crash模块
    Change-Id: I6bec5b3559fe8fa6ce42324ca54c5a458898f159

--- Instrument/sample/src/main/java/com/dunn/instrument/MainActivity.java
+++ Instrument/sample/src/main/java/com/dunn/instrument/MainActivity.java
@@ -1,7 +1,9 @@
 package com.dunn.instrument;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.dunn.instrument.excel.ExcelHelp;
@@ -11,23 +13,31 @@ import com.dunn.instrument.logger.ReleaseJointPoint;
 import com.dunn.instrument.logger.UploadJointPoint;
 
 public class MainActivity extends Activity {
+    public static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+        ApiCrash.crashInit(getApplication());
     }
 
     public void click(View view) {
+        Log.i(TAG,"click:");
         ApiLogger.logOut("logger日志测试");
         ApiExcel.excelInfo();
         ApiExcel.excelSubmit();
+        jump();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
         ApiLogger.loggerRelease();
+    }
+
+    public void jump() {
+        Intent intent = new Intent(this,SecondActivity.class);
+        startActivity(intent);
     }
 }
