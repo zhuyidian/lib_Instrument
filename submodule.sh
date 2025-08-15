eaffd63dc7e35cb4af73e7d65157a06b2c50a5e3
zhuyidian <zhuyidian@coocaa.com>
2025-08-15 11:01:38 +0800

    添加webview新仓库
    Change-Id: I9ff485d100ecfe0e23931c7b99f21c5494bc0e35

--- submodule.sh
+++ submodule.sh
@@ -19,7 +19,7 @@ submodules=(
 #    "http://gitlab.skysri.com/CCSDK/AppDownload library/LibApkDownload/AppDownload cooui-v1.8"
 #    "https://github.com/zhuyidian/lib_Instrument_Promotion Instrument/library/LibPromotion/Promotion main"
 #	"https://github.com/zhuyidian/lib_Instrument_Frameworks Instrument/library/LibFrameworks/Frameworks main"
-	"https://github.com/zhuyidian/lib_Instrument_Webview Instrument/library/LibWebview/Webview main"
+	"https://github.com/zhuyidian/lib_Instrument_WebviewSenior Instrument/library/LibWebviewSenior/WebviewSenior main"
 )
 
 function addSubmodule(){
